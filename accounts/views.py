b3ee750637263d511a8bc408efc8f1cb870135f5
Kamula <kamulaisaac@gmail.com>
2022-03-16 20:56:10 +0300

    custom access token

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs) 
+        data['first_name'] = self.user.first_name        
+        data['last_name'] = self.user.last_name
+        data['email'] = self.user.email
+        data['phone_number'] = self.user.phone_number    
+        
+        return data
+    
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    
+    serializer_class = MyTokenObtainPairSerializer
